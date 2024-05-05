397002c0952217f46034eddcaaba25b7d11885df
_tuquyennnn_ <quyentu2811@gmail.com>
2024-05-06 01:31:39 +0700

    cryxn

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -34,7 +34,7 @@ def training_pipeline(args: argparse.Namespace):
 
         # optimizer LoRA
         optimizer = torch.optim.Adam([
-            {'params': model.lora_adaptations.parameters(), 'lr': 1e-4},
+            {'params': model.lora_apdaptations.parameters(), 'lr': 1e-4},
             {'params': model.lora_revert.parameters(), 'lr': 1e-4},
             {'params': model.base_model.parameters(), 'lr': 5e-5}
             ], lr=5e-5)
