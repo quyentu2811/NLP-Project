ac8ed3a1405d8ba44a67822045abae76ed719a83
_tuquyennnn_ <quyentu2811@gmail.com>
2024-05-10 04:54:05 +0700

    update requirements.txt

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ echo "Chung toi dang setup moi truong ..."
 echo "..."
 pip install -q --upgrade pip
 pip install -q -U datasets
+pip install -q -r "/kaggle/working/NLP-Project/requirements.txt"
 pip install -q -r "/kaggle/working/dialogue-text-summarization/requirements.txt"
 pip install -q -r "/content/dialogue-text-summarization/requirements.txt"
 pip install -q -r "requirements.txt"
