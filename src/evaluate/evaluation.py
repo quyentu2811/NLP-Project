62b485439075b5e4db3b3398fcfa4d7059ad1e88
_tuquyennnn_ <quyentu2811@gmail.com>
2024-05-14 02:49:23 +0700

    /

--- src/evaluate/evaluation.py
+++ src/evaluate/evaluation.py
@@ -40,14 +40,16 @@ def evaluation_rouge(model: GeneralModel, data: Dataset) -> dict:
 
     model_summaries = []
 
+    tokenizer = model.base_model.config.tokenizer
+
     prefix = "Summarize the following conversation:\n\n"
     suffix = "\n\nSummary: "
 
     for idx, dialogue in enumerate(dialogues):
-        input = prefix + dialogue + suffix
-
-        output_text = model.generate(input)
-
+        input_text = prefix + dialogue + suffix
+        input_ids = tokenizer.encode(input_text, return_tensors="pt").to(model.device)
+        output_ids = model.generate(input_ids)
+        output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
         model_summaries.append(output_text)
 
     rouge_evaluator = RougeEvaluation()
