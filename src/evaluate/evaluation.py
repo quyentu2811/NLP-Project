6adf91be36c28bbaef01fd731b3b131b86e3761a
_tuquyennnn_ <quyentu2811@gmail.com>
2024-05-18 19:45:46 +0700

    change something

--- src/evaluate/evaluation.py
+++ src/evaluate/evaluation.py
@@ -48,8 +48,7 @@ def evaluation_rouge(model: GeneralModel, data: Dataset) -> dict:
 
     for idx, dialogue in enumerate(dialogues):
         input_text = prefix + dialogue + suffix
-        output_ids = model.generate(input_text=input_text)
-        output_text = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+        output_text = model.generate(input_text=input_text)
         model_summaries.append(output_text)
 
     rouge_evaluator = RougeEvaluation()
